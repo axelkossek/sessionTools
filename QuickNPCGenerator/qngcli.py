3ad13d6107f35e32bf07ec7174ece99dce23fec5
Axel Kossek <axelkossek@gmail.com>
2022-08-25 00:07:29 +0200

    added basic cli

--- QuickNPCGenerator/qngcli.py
+++ QuickNPCGenerator/qngcli.py
@@ -1,5 +1,10 @@
+import argparse
 from sessionmanager import SessionManager
 from statsrandomizer import StatsRandomizer
+import json
+
+RACES_FILE_DEFAULT = "RandomizationFiles/races.json"
+OCCUPATION_FILE_DEFAULT = "RandomizationFiles/occupations.json"
 
 def GenerateNPCDict(name, family, race, occupation, stats, appearance):
     return {
@@ -11,21 +16,63 @@ def GenerateNPCDict(name, family, race, occupation, stats, appearance):
         'appearance' : appearance
         }
 
-def GenerateStatsDict(abilityscores):
+def GenerateScoresDict(scores):
     return {
-        'str' : abilityscores[0],
-        'dex' : abilityscores[1],
-        'con' : abilityscores[2],
-        'int' : abilityscores[3],
-        'wis' : abilityscores[4],
-        'cha' : abilityscores[5]
+        'str' : scores[0],
+        'dex' : scores[1],
+        'con' : scores[2],
+        'int' : scores[3],
+        'wis' : scores[4],
+        'cha' : scores[5]
         }
 
-fileS = "test_1.json";
-manager = SessionManager(sessionFile = fileS, campaignName = "test", sessionNumber = 1)
+def ApplyRacialBonuses(scores, race, racesFile = RACES_FILE_DEFAULT):
+    with open(racesFile, "r") as f:
+            races = json.load(f)
+    return [sum(tup) for tup in zip(scores, races[race]["score_bonuses"])]
 
-#enpec1 = GenerateNPCDict("John", "Smith", "Human", "Smith", GenerateStatsDict(GenerateRandomAbilityScores()), "old")
-#enpec2 = GenerateNPCDict("Jane", "Novak", "Elf", "Wizard", GenerateStatsDict(GenerateRandomAbilityScores()), "young")
+def ApplyOccupationBonuses(scores, occupation, occupationFile = OCCUPATION_FILE_DEFAULT):
+    with open(occupationFile, "r") as f:
+            occupations = json.load(f)
+    return [sum(tup) for tup in zip(scores, occupations[occupation]["score_bonuses"])]
 
-statsRand = StatsRandomizer()
-scores = GenerateStatsDict(statsRand.GenerateRandomAbilityScores())
+
+
+#fileS = "test_1.json";
+#manager = SessionManager(sessionFile = fileS, campaignName = "test", sessionNumber = 1)
+
+##enpec1 = GenerateNPCDict("John", "Smith", "Human", "Smith", GenerateStatsDict(GenerateRandomAbilityScores()), "old")
+##enpec2 = GenerateNPCDict("Jane", "Novak", "Elf", "Wizard", GenerateStatsDict(GenerateRandomAbilityScores()), "young")
+
+def main():
+    parser = argparse.ArgumentParser(description = "A npc generator!")
+
+    parser.add_argument("-s", "--scores", type = str, nargs = 1,
+                        metavar = "0,0,0,0,0,0", default = None,
+                        help = "Set scores instead of random.")
+
+    parser.add_argument("-r", "--race", type = str, nargs = 1,
+                        metavar = "race", default = None,
+                        help = "Chooses race for the generator.")
+    
+    parser.add_argument("-o", "--occupation", type = str, nargs = 1,
+                        metavar = "occupation", default = "commoner",
+                        help = "Chooses race for the generator.")
+
+    args = parser.parse_args()
+
+    statsRand = StatsRandomizer()
+    scores = statsRand.GenerateRandomAbilityScores()
+
+    if args.scores != None:
+        scores = [int(i) for i in args.scores[0].split(',')]
+    if args.race != None:
+        scores = ApplyRacialBonuses(scores, args.race[0])
+    if args.occupation != None:
+        scores = ApplyOccupationBonuses(scores, args.occupation[0])
+
+    scoreDict = GenerateScoresDict(scores)
+    print(scoreDict)
+
+if __name__ == "__main__":
+    main()
